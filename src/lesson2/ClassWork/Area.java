fbc9f7e21e563098b0f753d31de5f50436b9f28e
kosya54 <kosya54@gmail.com>
2019-04-21 12:22:44 +0700

    Convert println to printf.

--- src/lesson2/ClassWork/Area.java
+++ src/lesson2/ClassWork/Area.java
@@ -6,6 +6,7 @@ public class Area {
     }
 
     private static void printAreaCircle() {
+//        double radius = Math.toRadians(50);
         int radius = 50;
         System.out.printf("Вычисляет площадь  круга по формуле S = PI*R^2%n");
         System.out.printf("Площадь круга = %.3f при радиусе равном: %d%n", (Math.PI * Math.pow(radius, 2)), radius);
