2fa55ec6352de148127dbabdaf79f005fbd84638
kosya54 <kosya54@gmail.com>
2019-05-05 12:31:40 +0700

    Добавлен урок 6

---
+++ src/lesson6/HomeWork/Temperature.java
@@ -0,0 +1,25 @@
+package lesson6.HomeWork;
+
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите температуру в градусах цельсия:");
+        int enteredTemperature = scanner.nextInt();
+
+        System.out.printf("%d градусов цельсия равно %.2f градусов кельвина%n", enteredTemperature, convertToKelvin(enteredTemperature));
+        System.out.printf("%d градусов цельсия равно %.2f градусов по фаренгейту", enteredTemperature, convertToFahrengeit(enteredTemperature));
+
+    }
+
+    private static double convertToKelvin(int temperature) {
+        return temperature + 273.15;
+    }
+
+    private static double convertToFahrengeit(int temperature) {
+        return (temperature * 9 / 5) + 32;
+    }
+}
+
