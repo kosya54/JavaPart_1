76fcbabf9352a88cd58a7116d72c4256ff6fe65b
kosya54 <49726304+kosya54@users.noreply.github.com>
2019-05-21 13:20:59 +0300

    Update Deposit.java

--- src/CourseWork/Deposit/Deposit.java
+++ src/CourseWork/Deposit/Deposit.java
@@ -1,7 +1,6 @@
 package CourseWork.Deposit;
 
 import java.util.Scanner;
-import java.util.Calendar;
 
 public class Deposit {
     public static void main(String[] args) {
@@ -13,21 +12,8 @@ public class Deposit {
             depositAmount = scanner.nextDouble();
 
             double epsilon = 1.0e-10;
-
             if (depositAmount <= epsilon) {
                 System.out.println("Сумма не может быть меньше или равна нулю.");
-            } else {
-                break;
-            }
-        }
-
-        int startMonth;
-        while (true) {
-            System.out.println("Укажите с какого месяца делается вклад: ");
-            startMonth = scanner.nextInt();
-
-            if (startMonth <= 0 || startMonth > 12) {
-                System.out.println("Не корректный месяц.");
             } else {
                 break;
             }
@@ -45,44 +31,16 @@ public class Deposit {
             }
         }
 
-        Calendar calendar = Calendar.getInstance();
-
-        int daysCount = 31;
-        int daysInYear = 365;
-        int currentYear = calendar.get(Calendar.YEAR);
-        switch (startMonth) {
-            case 2:
-                if (isLeapYear(currentYear)) {
-                    daysCount = 29;
-                    daysInYear = 366;
-                } else {
-                    daysCount = 28;
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                daysCount = 30;
-                break;
-        }
-
-        int annualInterestRate = 14;
-        double deposit = calculateProfit(depositAmount, annualInterestRate, daysCount, daysInYear, monthCount);
-        System.out.printf("Сумма вклада с учетом процентов за %d месяцев при ставке в %d%% годовых составит: %.2f рублей.%n", monthCount, annualInterestRate, deposit);
+        final double annualInterestRate = 10.5;
+        double deposit = calculateProfit(annualInterestRate, depositAmount, monthCount);
+        System.out.printf("Сумма вклада с учетом процентов за %d мес. при ставке %.2f%% годовых составит: %.2f рублей.%n", monthCount, annualInterestRate, deposit);
         System.out.printf("Прибыль за данный период составляет: %.2f рублей.", deposit - depositAmount);
     }
 
-    private static double calculateProfit(double deposit, int annualInterestRate, int daysCount, int daysInYear, int monthCount) {
-        double sum;
-        for (int i = 1; i <= monthCount; i++) {
-            sum = ((deposit * annualInterestRate * daysCount) / daysInYear) / 100;
-            deposit += sum;
-        }
-        return deposit;
-    }
-
-    private static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    private static double calculateProfit(double annualInterestRate, double depositAmount, int monthCount) {
+        final int daysCount = 30;
+        final int daysInYear = 365;
+        
+        return depositAmount * Math.pow((1 + (annualInterestRate * daysCount) / (100 * daysInYear)), monthCount);
     }
 }
