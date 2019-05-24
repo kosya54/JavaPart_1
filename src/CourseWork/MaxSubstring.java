f2ab002617beaa181ba16c20400c78f9b3897ab3
kosya54 <49726304+kosya54@users.noreply.github.com>
2019-05-24 12:07:55 +0300

    Update MaxSubstring.java

--- src/CourseWork/MaxSubstring.java
+++ src/CourseWork/MaxSubstring.java
@@ -2,21 +2,20 @@ package CourseWork;
 
 public class MaxSubstring {
     public static void main(String[] args) {
-        String string = "Baa Bddddddddf cfb fdyyyyyyygfgfn dfj ghj fgjf gfhj gj llllllllllllLlllLlllLLllllllllll ghjghfj gj fghjgjhgjg ghjhgjh ghjghhjgf gjh ghjgf gj gjhfg ";
+        String string = "aaaaaaabv dfveeeees dfssdg dfgZZzzzZZzzZ fghdfghdf";
         System.out.printf("В строке \"%s\"%n длина максимальной подстроки состоящей из одного символа равна: %d", string, findMaxSubstringLength(string));
     }
-
+//TODO: Сделать игнорирование пробелов и т.д.
     private static int findMaxSubstringLength(String string) {
         int maxSubstringLength = 0;
-        for (int i = 0; i < string.length(); i++) {
-            int substringLengthCount = 1;
-            for (int j = i + 1; j < string.length(); j++) {
-                if (string.toLowerCase().charAt(i) == string.toLowerCase().charAt(j)) {
-                    ++substringLengthCount;
-                } else {
-                    break;
-                }
+        int substringLengthCount = 1;
+        for (int i = 1; i < string.length(); i++) {
+            if (string.toLowerCase().charAt(i - 1) == string.toLowerCase().charAt(i)) {
+                ++substringLengthCount;
+            } else {
+                substringLengthCount = 1;
             }
+            
             if (maxSubstringLength < substringLengthCount) {
                 maxSubstringLength = substringLengthCount;
             }
