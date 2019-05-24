6f1655941bce11b9b04ed4b2b58e5fc834d139d8
kosya54 <49726304+kosya54@users.noreply.github.com>
2019-05-24 08:26:09 +0300

    Create Power.java

---
+++ src/lesson10/ClassWork/Power.java
@@ -0,0 +1,24 @@
+package lesson10.ClassWork;
+
+import java.util.Scanner;
+
+class Power {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Введите степень: ");
+        int power = scanner.nextInt();
+
+        System.out.printf("%d в степени %d равно: %d", number, power, getPower(number, power));
+    }
+
+    private static int getPower(int number, int power) {
+        if (power != 1) {
+            return number * getPower(number, power - 1);
+        }
+        return number;
+    }
+}
