ce81846aa846841805b9a433fbfea8953273a7dc
kosya54 <49726304+kosya54@users.noreply.github.com>
2019-05-24 13:19:53 +0300

    Update MaxSubstring.java

--- src/CourseWork/MaxSubstring.java
+++ src/CourseWork/MaxSubstring.java
@@ -2,15 +2,19 @@ package CourseWork;
 
 public class MaxSubstring {
     public static void main(String[] args) {
-        String string = "aaaaaaabv dfveeeees dfssdg dfgZZzzzZZzzZ fghdfghdf";
+        String string = "Aaaaaaabv dfveeeees dfssdg dfgZZzzzZZzzZ fghdfghdf";
+        
         System.out.printf("В строке \"%s\"%n длина максимальной подстроки состоящей из одного символа равна: %d", string, findMaxSubstringLength(string));
+        System.out.println();
     }
-//TODO: Сделать игнорирование пробелов и т.д.
+
     private static int findMaxSubstringLength(String string) {
+        string = string.toLowerCase();
+
         int maxSubstringLength = 0;
         int substringLengthCount = 1;
         for (int i = 1; i < string.length(); i++) {
-            if (string.toLowerCase().charAt(i - 1) == string.toLowerCase().charAt(i)) {
+            if (string.charAt(i - 1) == string.charAt(i)) {
                 ++substringLengthCount;
             } else {
                 substringLengthCount = 1;
